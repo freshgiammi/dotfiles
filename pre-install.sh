175583c94139d2d441fd0c772f79081a14cf42f2
Gianmarco Rengucci (freshgiammi) <rengucci.gianmarco@gmail.com>
2021-12-08 19:58:51 +0100

    Push local changes w/ dotbot

---
+++ pre-install.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Change default shell
+if [! $0 = "-zsh"]; then
+  echo 'Changing default shell to zsh'
+  chsh -s /bin/zsh
+else
+  echo 'Already using zsh'
+fi
+
+# Check if oh-my-zsh is installed
+OMZDIR="$HOME/.oh-my-zsh"
+if [ ! -d "$OMZDIR" ]; then
+  echo 'Installing oh-my-zsh'
+  /bin/sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+else
+  echo 'Updating oh-my-zsh'
+  omz update
+fi
